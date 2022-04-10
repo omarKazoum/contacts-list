1de9652c1bb25c4047b5199ae3c36eb1683d3404
omar kazoum <omarKazoum96@gmail.com>
2022-04-10 12:21:24 +0000

    added password2 which is only used for passwords match

--- include/Constants.php
+++ include/Constants.php
@@ -9,6 +9,10 @@ class Constants
     public const Users_Col_UserName='name';
     public const Users_Col_PasswordHash='pass_hash';
     public const Users_Password='pass';
+    //not stored
+    public const Users_Password2='pass2';
+
+    //for remember me option
     public const Session_RememberMe='rememberme';
     public const Session_RememberMe_Email='rememberme_email';
     public const Session_RememberMe_Pass='rememberme_pass';
