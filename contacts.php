b11b86e304263b2d59c06c567ab6290e2fcf2b3e
omar kazoum <omarKazoum96@gmail.com>
2022-04-10 12:14:50 +0000

    added login.php

--- contacts.php
+++ contacts.php
@@ -11,25 +11,25 @@ require_once "templates/header.php" ?>
                 <div class="form-group">
                     <label for="<?=Constants::Users_Col_UserName?>" class="form-label">Username</label>
                     <input
-                            id="<?=Constants::Users_Col_UserName?>"
-                            type="text"
-                            class="form-control"
-                            data-validate-pattern="\w{3,}"
-                            data-validate-message="Please fill in a valid user name"
-                            placeholder="Username"
-                            name="<?=Constants::Users_Col_UserName?>">
+                        id="<?=Constants::Users_Col_UserName?>"
+                        type="text"
+                        class="form-control"
+                        data-validate-pattern="\w{3,}"
+                        data-validate-message="Please fill in a valid user name"
+                        placeholder="Username"
+                        name="<?=Constants::Users_Col_UserName?>">
                 </div>
                 <div class="form-group">
                     <label for="<?=Constants::Users_Password?>" class="form-label">Password</label>
                     <input
-                            placeholder="Password"
-                            id="<?=Constants::Users_Password?>"
-                            type="password"
-                            class="form-control"
-                            data-validate-pattern=".{8,}"
-                            data-validate-message="Passwords do not match"
-                            data-validate="1"
-                            name="<?=Constants::Users_Password?>">
+                        placeholder="Password"
+                        id="<?=Constants::Users_Password?>"
+                        type="password"
+                        class="form-control"
+                        data-validate-pattern=".{8,}"
+                        data-validate-message="Passwords do not match"
+                        data-validate="1"
+                        name="<?=Constants::Users_Password?>">
                 </div>
                 <button class="btn btn-primary w-100 my-2" type="submit">
                     Login
