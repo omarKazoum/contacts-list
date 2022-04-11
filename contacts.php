69d01b32a9be7b8f98f5aa59de07e163597bebb5
omar kazoum <omarKazoum96@gmail.com>
2022-04-11 09:41:28 +0000

    finished front-end part of the app

--- contacts.php
+++ contacts.php
@@ -5,40 +5,101 @@ require_once "templates/header.php" ?>
     <div class="row">
         <div class="content col-12 col-md-6 offset-md-3">
             <h1 class="page-header text-align-center w-100">
-                Authenticate
+                Contacts
             </h1>
-            <form class="">
-                <div class="form-group">
-                    <label for="<?=Constants::Users_Col_UserName?>" class="form-label">Username</label>
-                    <input
-                        id="<?=Constants::Users_Col_UserName?>"
-                        type="text"
-                        class="form-control"
-                        data-validate-pattern="\w{3,}"
-                        data-validate-message="Please fill in a valid user name"
-                        placeholder="Username"
-                        name="<?=Constants::Users_Col_UserName?>">
+            <div class="contacts-list row">
+                <h2 class="page-header text-align-center w-100 border-bottom border-1">
+                    Contacts list
+                </h2>
+                <div class="no-contacts-msg fw-light">
+                    No contacts.
                 </div>
-                <div class="form-group">
-                    <label for="<?=Constants::Users_Password?>" class="form-label">Password</label>
-                    <input
-                        placeholder="Password"
-                        id="<?=Constants::Users_Password?>"
-                        type="password"
-                        class="form-control"
-                        data-validate-pattern=".{8,}"
-                        data-validate-message="Passwords do not match"
-                        data-validate="1"
-                        name="<?=Constants::Users_Password?>">
-                </div>
-                <button class="btn btn-primary w-100 my-2" type="submit">
-                    Login
-                </button>
-                <p class="m-1 text-below-form">
-                    No account? <a href="signup.php" class="link">Sign up</a> here.
-                </p>
+                <table class="table">
+                    <tr>
+                        <td class="fw-bold contact-name">contact name</td>
+                        <td class="contact-email">contact@mail.com</td>
+                        <td class="contact-phone">+212727783888</td>
+                        <td class="contact-adress">some demo adress</td>
+                        <td class="contact-actions d-flex flex-column">
+                            <a href="#form-con" class="link edit-btn">Edit</a>
+                            <a href="" class="link delete-btn">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="fw-bold contact-name">contact name</td>
+                        <td class="contact-email">contact@mail.com</td>
+                        <td class="contact-phone">+212727783888</td>
+                        <td class="contact-adress">some demo adress</td>
+                        <td class="contact-actions d-flex flex-column">
+                            <a href="#form-con" class="link edit-btn">Edit</a>
+                            <a href="" class="link delete-btn">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="fw-bold contact-name">contact name</td>
+                        <td class="contact-email">contact@mail.com</td>
+                        <td class="contact-phone">+212727783888</td>
+                        <td class="contact-adress">some demo adress</td>
+                        <td class="contact-actions d-flex flex-column">
+                            <a href="#form-con" class="link edit-btn">Edit</a>
+                            <a href="" class="link delete-btn">Delete</a>
+                        </td>
+                    </tr>
 
-            </form>
+                </table>
+            </div>
+            <div id="form-con">
+                <form class="needs-validation container-fluid">
+                    <div class="row mt-2">
+                        <div class="form-group col-6">
+                            <label for="<?=Constants::Contacts_Col_Name?>" class="form-label">Name</label>
+                            <input
+                                    id="<?=Constants::Contacts_Col_Name?>"
+                                    type="text"
+                                    class="form-control"
+                                    data-validate-pattern="^\w{3,}$"
+                                    data-validate-message="Invalid he name is mandatory"
+                                    placeholder="contact Name"
+                                    name="<?=Constants::Contacts_Col_Name?>">
+                        </div>
+                        <div class="form-group col-6">
+                            <label for="<?=Constants::Contacts_Col_Phone?>" class="form-label">Phone</label>
+                            <input
+                                    id="<?=Constants::Contacts_Col_Phone?>"
+                                    type="text"
+                                    class="form-control"
+                                    data-validate-pattern="^\+?(212)|[6589]\d{8}$"
+                                    data-validate-message="The phone number"
+                                    placeholder="contact phone number"
+                                    name="<?=Constants::Contacts_Col_Phone?>">
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="form-group col-6">
+                            <label for="<?=Constants::Contacts_Col_Email?>" class="form-label">Email</label>
+                            <input
+                                    id="<?=Constants::Contacts_Col_Email?>"
+                                    type="text"
+                                    class="form-control"
+                                    data-validate-pattern="^[a-zA-Z\-]+@[a-zA-Z\-]+(\.[a-zA-Z\-]+)+$"
+                                    data-validate-message="The email is invalid"
+                                    placeholder="example@domain.com"
+                                    name="<?=Constants::Contacts_Col_Email?>">
+                        </div>
+                        <div class="form-group col-6">
+                            <label for="<?=Constants::Contacts_Col_Adress?>" class="form-label">Adress</label>
+                            <input
+                                    id="<?=Constants::Contacts_Col_Adress?>"
+                                    type="text"
+                                    class="form-control"
+                                    data-validate-pattern=".*"
+                                    data-validate-message=""
+                                    placeholder="Optional: your contact adress"
+                                    name="<?=Constants::Contacts_Col_Adress?>">
+                        </div>
+                    </div>
+                </form>
+            </div>
 
         </div>
     </div>
